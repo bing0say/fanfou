651ac4b15ba1a3d138ad37253deab0f8d169c8e3
bingosay <bingosay@icloud.com>
2016-04-16 20:33:35 +0800

    remove fanfou/

--- fanfou/README.md
+++
@@ -1,2 +0,0 @@
-# fanfou
-A spider crawl Allen Zhang's news on fanfou.com
--- fanfou/manage.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-__author__ = 'jiang'
-__creattime__ = '2016/3/6 20:36'
-from scrapy.cmdline import execute
-
-execute()
